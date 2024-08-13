b70b6e03b0523c27978b422acb0e24e36723024f
SAVVY1710 <163214365+SAVVY1710@users.noreply.github.com>
2024-08-12 19:04:42 -0700

    changed idea because keypad was not working

--- sketch.ino
+++ sketch.ino
@@ -3,24 +3,10 @@
 
 LiquidCrystal lcd(12, 9, 5, 10, 7,6);
 
-// Define the number of rows and columns in the keypad
-const uint8_t ROWS = 4;
-const uint8_t COLS = 4;
-
-// Define the key map for the keypad
-char keys[ROWS][COLS] = {
-  {'1', '2', '3', 'A'},
-  {'4', '5', '6', 'B'},
-  {'7', '8', '9', 'C'},
-  {'*', '0', '#', 'D'}
-};
+const int buttonPin = 4;
+int oldValue = HIGH; // default/idle value for pin 4 is high.
 
 // Define the pin numbers for the columns and rows
-uint8_t colPins[COLS] = {21, 20, 19, 18}; // C1, C2, C3, C4
-uint8_t rowPins[ROWS] = {28, 27, 26, 22}; // R1, R2, R3, R4
-
-// Initialize the keypad
-Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
 const int relayPin = 4; // Define the relay pin
 
@@ -30,19 +16,30 @@ void setup() {
   lcd.begin(16, 2);
   // you can now interact with the LCD, e.g.:
   lcd.print("Hello World!");
+  Serial1.println("Hello World!");
 }
 
 void loop() {
-  char key = keypad.getKey();  // Get the key pressed
+  // Read the value of pin 4.
+  int newValue = digitalRead(buttonPin);
 
-  if (key != NO_KEY) {  // If a key is pressed
-    Serial1.println(key);  // Print the key to the serial monitor
-
-    // Control relay based on key press (example)
-    if (key == 'A') {
-      digitalWrite(relayPin, HIGH);  // Turn relay ON
-    } else if (key == 'B') {
-      digitalWrite(relayPin, LOW);   // Turn relay OFF
+  // Check if the value was changed,
+  // by comparing it with the previous value.
+  if(newValue != oldValue)
+  {
+    if(newValue == LOW)
+    {
+      Serial.println("The button is pressed.");
     }
+    else
+    {
+      Serial.println("The button is released.");
+    }
+    // Remember the value for the next time.
+    oldValue = newValue;
   }
+
+  // Slow down the sketch.
+  // Also for debouncing the button.
+  delay(100);
 }
